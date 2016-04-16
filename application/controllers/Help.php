93e64ab58e80b8a3e915b520bc6a2e12af5ae6bb
Whale <paul.feuvraux@gmail.com>
2016-04-16 16:33:26 +0200

    remove

--- application/controllers/Help.php
+++
@@ -1,8 +0,0 @@
-<?php
-    class Help extends Controleur {
-        
-        function DefaultAction() {
-            include_once('./application/views/vHelp.php');
-        }
-    }
-?>
--- readme.md
+++
@@ -1,3 +0,0 @@
-This is the full code of the feature website.
-
-You can fork it or contribute when you want ! :)
